454dc8e2cc21f3c98a24aa2374882b10f708b39f
Aaron Wood <aaronjwood@gmail.com>
2016-11-17 18:19:56 -0600

    Modify access levels

--- individual/individual.go
+++ individual/individual.go
@@ -1,7 +1,6 @@
 package individual
 
 import (
-	"fmt"
 	"math/rand"
 	"time"
 )
@@ -22,7 +21,7 @@ func New(size int64) *Individual {
 }
 
 //Gets the current fitness level
-func (i *Individual) getFitness() int64 {
+func (i *Individual) GetFitness() int64 {
 	return i.fitness
 }
 
@@ -33,11 +32,11 @@ func (i *Individual) setFitness(fitness int64) *Individual {
 }
 
 //Gets a gene at a specified index
-func (i *Individual) getGene(idx int64) int64 {
+func (i *Individual) GetGene(idx int64) int64 {
 	return i.genes[idx]
 }
 
-func (i *Individual) getGenes() []int64 {
+func (i *Individual) GetGenes() []int64 {
 	return i.genes
 }
 
@@ -49,7 +48,7 @@ func (i *Individual) setGene(idx, gene int64) *Individual {
 }
 
 //Generates random genes and fills up the gene pool
-func (i *Individual) generateGenes() *Individual {
+func (i *Individual) GenerateGenes() *Individual {
 	for c := range i.genes {
 		i.setGene(int64(c), i.generator.Int63n(2))
 	}
@@ -58,18 +57,18 @@ func (i *Individual) generateGenes() *Individual {
 }
 
 //Mutates a gene by flipping it
-func (i *Individual) mutate() *Individual {
+func (i *Individual) Mutate() *Individual {
 	idx := i.generator.Int63n(int64(len(i.genes)))
-	i.setGene(idx, 1-i.getGene(idx))
+	i.setGene(idx, 1-i.GetGene(idx))
 
 	return i
 }
 
 //Determines a fitness value based on all genes
-func (i *Individual) evaluateFitness() int64 {
+func (i *Individual) EvaluateFitness() int64 {
 	i.fitness = 0
 	for c := range i.genes {
-		i.fitness += i.getGene(int64(c))
+		i.fitness += i.GetGene(int64(c))
 	}
 
 	return i.fitness
