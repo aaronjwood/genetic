a0178946279ac1351329ddd3f68a7e492a819ac9
Aaron Wood <aaronjwood@gmail.com>
2016-11-17 18:24:15 -0600

    Start building the population

---
+++ population/population.go
@@ -0,0 +1,27 @@
+package population
+
+import (
+	"geneticalgos/individual"
+	"math/rand"
+)
+
+type Population struct {
+	Elite          int
+	PopulationSize int
+	Rounds         int
+	Mutation       float64
+	Crossover      float64
+	generator      *rand.Rand
+	population     []individual.Individual
+	fitness        float64
+}
+
+func New(size int) *Population {
+	pop := &Population{
+		population: make([]individual.Individual, size),
+	}
+
+	for i := range pop.population {
+		append(pop.population, individual.New(1000).GenerateGenes())
+	}
+}
