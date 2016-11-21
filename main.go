5a4a702eef65570932e895ffd124f8a96c9e8077
Aaron Wood <aaronjwood@gmail.com>
2016-11-21 03:08:13 -0500

    Support finding the best individual of the population, minor fixes and tweaks

--- main.go
+++ main.go
@@ -1,18 +1,16 @@
 package main
 
 import (
-	"github.com/aaronjwood/genetic/individual"
-	"github.com/aaronjwood/genetic/population"
 	"fmt"
+	"github.com/aaronjwood/genetic/population"
 )
 
 func main() {
 	const size int64 = 10000
 
 	pop := population.New(size)
-	indvs := individual.New(size)
 
 	fmt.Println(pop.GetFitness())
-	fmt.Println(indvs.GetFitness())
+	fmt.Println(pop.BestIndividual(population.Maximization).EvaluateFitness())
 
 }
--- population/population.go
+++ population/population.go
@@ -6,21 +6,25 @@ import (
 	"time"
 )
 
+const (
+	Minimization = iota
+	Maximization
+)
+
 type Population struct {
-	Elite          int
-	Rounds         int
-	Mutation       float64
-	Crossover      float64
-	generator      *rand.Rand
-	population     []individual.Individual
-	fitness        int64
+	Elite      int
+	Rounds     int
+	Mutation   float64
+	Crossover  float64
+	generator  *rand.Rand
+	population []individual.Individual
+	fitness    int64
 }
 
 //Creates a new population
 func New(size int64) *Population {
 	pop := &Population{
-		population: make([]individual.Individual, size),
-		generator:  rand.New(rand.NewSource(time.Now().UnixNano())),
+		generator: rand.New(rand.NewSource(time.Now().UnixNano())),
 	}
 
 	for i := int64(0); i < size; i++ {
@@ -49,14 +53,14 @@ func (p *Population) getPopulation() []individual.Individual {
 }
 
 //Selects an individual at random
-func (p *Population) rouletteSelection() individual.Individual {
+func (p *Population) rouletteSelection() *individual.Individual {
 	selection := p.generator.Int63() * p.fitness
 	var idx int
 	for idx = 0; idx < len(p.population) && selection > 0; idx++ {
 		selection -= p.population[idx].GetFitness()
 	}
 
-	return p.population[idx-1]
+	return &p.population[idx-1]
 }
 
 //Determines the fitness level for the entire population
@@ -67,4 +71,39 @@ func (p *Population) evaluate() int64 {
 	}
 
 	return p.fitness
+}
+
+//Finds the best individual based on using minimization or maximization
+func (p *Population) BestIndividual(algo int) *individual.Individual {
+	var idxMin, idxMax int64 = 0, 0
+	var currentMin, currentMax int64 = 1, 0
+	var currentVal int64
+
+	for idx := 0; idx < len(p.population); idx++ {
+		currentVal = p.population[idx].GetFitness()
+		if currentMax < currentMin {
+			currentMin = currentVal
+			currentMax = currentVal
+			idxMin = int64(idx)
+			idxMax = int64(idx)
+		}
+
+		if currentVal > currentMax {
+			currentMax = currentVal
+			idxMax = int64(idx)
+		}
+
+		if currentVal < currentMin {
+			currentMin = currentVal
+			idxMin = int64(idx)
+		}
+	}
+
+	if algo == Minimization {
+		return &p.population[idxMin]
+	} else if algo == Maximization {
+		return &p.population[idxMax]
+	}
+
+	return &p.population[idxMax]
 }
